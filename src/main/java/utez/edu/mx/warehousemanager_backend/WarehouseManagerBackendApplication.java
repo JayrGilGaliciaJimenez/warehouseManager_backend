168077aabbb2034f74cd270b915104cc0d16b899
JayrGilGaliciaJimenez <20223tn052@utez.edu.mx>
2025-02-27 15:52:06 -0600

    first commit

---
+++ src/main/java/utez/edu/mx/warehousemanager_backend/WarehouseManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.warehousemanager_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseManagerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseManagerBackendApplication.class, args);
+    }
+
+}
