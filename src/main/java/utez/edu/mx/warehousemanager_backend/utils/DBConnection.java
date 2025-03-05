84b1f510c65c7ab2d63aa51b2866995b52edc00f
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2025-03-05 15:52:23 -0600

    feat: :zap: Se agregó un archivo de utilidades y uno para la conexión de la base de datos
    Se configuró el archivo de utilidades para regresar respuestas personalizadas en los servicios, así como también un archivo para la configuración de la base de datos

---
+++ src/main/java/utez/edu/mx/warehousemanager_backend/utils/DBConnection.java
@@ -0,0 +1,30 @@
+package utez.edu.mx.warehousemanager_backend.utils;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class DBConnection {
+    @Value("${db.url}")
+    private String dbUrl;
+
+    @Value("${db.username}")
+    private String dbUsername;
+
+    @Value("${db.password}")
+    private String dbPassword;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(dbUsername);
+        dataSource.setPassword(dbPassword);
+        return dataSource;
+    }
+}
