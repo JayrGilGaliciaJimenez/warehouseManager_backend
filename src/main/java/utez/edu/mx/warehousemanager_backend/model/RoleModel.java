3ef3bfdaa0398230801e9143f4c78dd7d412bc31
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2025-03-09 01:36:58 -0600

    feat(model): :zap: add RoleModel for role management

---
+++ src/main/java/utez/edu/mx/warehousemanager_backend/model/RoleModel.java
@@ -0,0 +1,34 @@
+package utez.edu.mx.warehousemanager_backend.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "role")
+public class RoleModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "role")
+    private Set<UserModel> users = new HashSet<>();
+}
