47a95e66b00021bbf8717d739711fe3877487287
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2025-03-09 01:37:26 -0600

    feat(model): :zap: add UserStatusModel for user status management

---
+++ src/main/java/utez/edu/mx/warehousemanager_backend/model/UserStatusModel.java
@@ -0,0 +1,34 @@
+package utez.edu.mx.warehousemanager_backend.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user_status")
+public class UserStatusModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "status")
+    private Set<UserModel> users = new HashSet<>();
+}
