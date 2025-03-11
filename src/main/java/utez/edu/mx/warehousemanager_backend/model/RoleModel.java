ec1da31d0f456abff7b4c495430bfca7d8c74bf0
JayrGilGaliciaJimenez <20223tn052@utez.edu.mx>
2025-03-10 23:26:49 -0600

    feat(model): Update RoleModel entity with UUID and creation date fields. The name of the table also change from role to roles.
    Author: JayrGil

--- src/main/java/utez/edu/mx/warehousemanager_backend/model/RoleModel.java
+++ src/main/java/utez/edu/mx/warehousemanager_backend/model/RoleModel.java
@@ -1,16 +1,12 @@
 package utez.edu.mx.warehousemanager_backend.model;
 
+import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,14 +17,23 @@ import lombok.Setter;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "role")
+@Table(name = "roles")
 public class RoleModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    private String uuid;
+    @Column(name = "creation_date", nullable = false, updatable = false)
+    private LocalDateTime creationDate;
     private String name;
+
 
     @JsonIgnore
     @OneToMany(mappedBy = "role")
     private Set<UserModel> users = new HashSet<>();
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "related_user_id")
+//    private UserModel relatedUser;
+
 }
