0f31b18f75b9e414d19f96215ba4afd5e09f64ef
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2025-03-16 22:46:56 -0600

    refactor(model): :fire: remove unused UUID attribute from ResetTokenModel
    Removed the unused UUID attribute from ResetTokenModel as it was not being utilized

--- src/main/java/utez/edu/mx/warehousemanager_backend/model/ResetTokenModel.java
+++ src/main/java/utez/edu/mx/warehousemanager_backend/model/ResetTokenModel.java
@@ -10,7 +10,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -27,19 +26,12 @@ public class ResetTokenModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private UUID uuid;
     private String token;
-    
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(nullable = true, name = "user_id")
     private UserModel user;
 
     private LocalDateTime expiryDate;
 
-    @PrePersist
-    public void generateUUID() {
-        if (uuid == null) {
-            uuid = UUID.randomUUID();
-        }
-    }
 }
