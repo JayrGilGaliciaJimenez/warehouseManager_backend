57aa2f85d2e1f4e31e019b6b49defed7f29b592f
JayrGilGaliciaJimenez <20223tn052@utez.edu.mx>
2025-03-17 15:14:36 -0600

    fix(config): InitialConfig
    creationDate was missing at the time user and admin role are being created
    
    Author: JayrGil

--- src/main/java/utez/edu/mx/warehousemanager_backend/config/InitialConfig.java
+++ src/main/java/utez/edu/mx/warehousemanager_backend/config/InitialConfig.java
@@ -11,6 +11,8 @@ import utez.edu.mx.warehousemanager_backend.model.UserModel;
 import utez.edu.mx.warehousemanager_backend.repository.IRoleRepository;
 import utez.edu.mx.warehousemanager_backend.repository.IUserRepository;
 
+import java.time.LocalDateTime;
+
 @Configuration
 @RequiredArgsConstructor
 public class InitialConfig implements CommandLineRunner {
@@ -32,12 +34,14 @@ public class InitialConfig implements CommandLineRunner {
         RoleModel adminRole = roleRepository.findByName("ROLE_ADMIN").orElseGet(() -> {
             RoleModel role = new RoleModel();
             role.setName("ROLE_ADMIN");
+            role.setCreationDate(LocalDateTime.now());
             return roleRepository.save(role);
         });
 
         RoleModel userRole = roleRepository.findByName("ROLE_USER").orElseGet(() -> {
             RoleModel role = new RoleModel();
             role.setName("ROLE_USER");
+            role.setCreationDate(LocalDateTime.now());
             return roleRepository.save(role);
         });
 
