45fcc57b4e4258398c3bbd67defea23d966b29d7
JayrGilGaliciaJimenez <20223tn052@utez.edu.mx>
2025-03-17 16:48:19 -0600

    feat(model): Add ProductOut entity with UUID, quantity, and pricing details Author:  JayrGil

---
+++ src/main/java/utez/edu/mx/warehousemanager_backend/model/ProductOut.java
@@ -0,0 +1,41 @@
+package utez.edu.mx.warehousemanager_backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "product_out")
+public class ProductOut {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+    private UUID uuid;
+    private String productName;
+    private int quantity;
+    private double unitPrice;
+    private double totalAmount;
+    private LocalDateTime outDate = LocalDateTime.now();
+    private String reciverName;
+    private Integer relatedUserId;
+
+
+
+    @PrePersist
+    public void generateUUID() {
+        if (uuid == null) {
+            uuid = UUID.randomUUID();
+        }
+    }
+
+}
