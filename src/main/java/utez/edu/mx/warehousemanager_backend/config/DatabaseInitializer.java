f7fa80df8425e108bee046979f9b32e533469f41
JayrGilGaliciaJimenez <20223tn052@utez.edu.mx>
2025-03-17 18:16:04 -0600

    feat(config): Triggers
    DatabaseInitializer file added to create the triggers automatically when the project starts.
    
    Author: JayrGil

---
+++ src/main/java/utez/edu/mx/warehousemanager_backend/config/DatabaseInitializer.java
@@ -0,0 +1,87 @@
+package utez.edu.mx.warehousemanager_backend.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseInitializer implements CommandLineRunner {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public DatabaseInitializer(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        String createProductEntriesToStockTrigger = """
+                
+                 CREATE TRIGGER IF NOT EXISTS product_entries_to_stock
+                    AFTER INSERT
+                    ON product_entries
+                    FOR EACH ROW
+                BEGIN
+                    DECLARE existing_quantity INT;
+                
+                    -- Check if the product already exists in the stock table
+                    SELECT quantity
+                    INTO existing_quantity
+                    FROM stock
+                    WHERE productName = NEW.productName
+                      AND measurementUnit = NEW.measurementUnit;
+                
+                    IF existing_quantity IS NOT NULL THEN
+                        -- If the product exists, update the quantity and totalAmount
+                    UPDATE stock
+                    SET quantity     = quantity + NEW.quantity,
+                        totalAmount = (quantity) * NEW.unitPrice
+                    WHERE productName = NEW.productName
+                      AND measurementUnit = NEW.measurementUnit;
+                    ELSE
+                        -- If the product does not exist, insert a new record with totalAmount
+                        INSERT INTO stock (id, productName, measurementUnit, quantity, unitPrice, totalAmount, suplierId)
+                        VALUES (NEW.id, NEW.productName, NEW.measurementUnit, NEW.quantity, NEW.unitPrice,
+                                NEW.quantity * NEW.unitPrice, NEW.suplierId);
+                END IF;
+                END;
+                """;
+
+        String createProductOutsToStockTrigger = """
+                
+                CREATE TRIGGER IF NOT EXISTS product_outs_to_stock
+                        AFTER INSERT
+                        ON product_outs
+                        FOR EACH ROW
+                    BEGIN
+                        DECLARE existing_quantity INT;
+                
+                        -- Check if the product already exists in the stock table
+                        SELECT quantity
+                        INTO existing_quantity
+                        FROM stock
+                        WHERE productName = NEW.productName
+                          AND measurementUnit = NEW.measurementUnit;
+                
+                        IF existing_quantity IS NOT NULL THEN
+                            -- If the product exists, update the quantity and totalAmount
+                        UPDATE stock
+                        SET quantity     = quantity - NEW.quantity,
+                            totalAmount = (quantity) * NEW.unitPrice
+                        WHERE productName = NEW.productName
+                          AND measurementUnit = NEW.measurementUnit;
+                        ELSE
+                            -- If the product does not exist, insert a new record with totalAmount
+                            INSERT INTO stock (id, productName, measurementUnit, quantity, unitPrice, totalAmount)
+                            VALUES (NEW.id, NEW.productName, NEW.measurementUnit, NEW.quantity, NEW.unitPrice,
+                                    NEW.quantity * NEW.unitPrice);
+                    END IF;
+                    END;
+                """;
+
+        jdbcTemplate.execute(createProductEntriesToStockTrigger);
+        jdbcTemplate.execute(createProductOutsToStockTrigger);
+
+    }
+}
