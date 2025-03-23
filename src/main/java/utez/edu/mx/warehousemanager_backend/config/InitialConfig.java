7480240af3760662b3649cb6a80d32a25709b379
JayrGilGaliciaJimenez <20223tn052@utez.edu.mx>
2025-03-22 19:08:59 -0600

    feat(InitialConfig): add user creation logic and additional user properties

--- src/main/java/utez/edu/mx/warehousemanager_backend/config/InitialConfig.java
+++ src/main/java/utez/edu/mx/warehousemanager_backend/config/InitialConfig.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
 import lombok.RequiredArgsConstructor;
 import utez.edu.mx.warehousemanager_backend.model.RoleModel;
 import utez.edu.mx.warehousemanager_backend.model.UserModel;
@@ -22,12 +21,35 @@ public class InitialConfig implements CommandLineRunner {
 
     @Value("${admin.name}")
     private String adminName;
-
+    @Value("${admin.lastname}")
+    private String adminLastname;
     @Value("${admin.email}")
     private String adminEmail;
-
     @Value("${admin.password}")
     private String adminPassword;
+
+    @Value("${user.name}")
+    private String userName;
+    @Value("${user.lastname}")
+    private String userLastname;
+    @Value("${user.email}")
+    private String userEmail;
+    @Value("${user.password}")
+    private String userPassword;
+
+
+    private void createUserIfNotExists(String name, String lastname, String email, String password, RoleModel role) {
+        if (userRepository.findByEmail(email) == null) {
+            UserModel user = new UserModel();
+            user.setName(name);
+            user.setLastname(lastname);
+            user.setEmail(email);
+            user.setPassword(passwordEncoder.encode(password));
+            user.setRole(role);
+            user.setStatus("Active");
+            userRepository.save(user);
+        }
+    }
 
     @Override
     public void run(String... args) throws Exception {
@@ -45,16 +67,8 @@ public class InitialConfig implements CommandLineRunner {
             return roleRepository.save(role);
         });
 
-        if (userRepository.findByEmail(adminEmail) == null) {
-            UserModel user = new UserModel();
-            user.setName(adminName);
-            user.setLastname(adminEmail);
-            user.setEmail(adminEmail);
-            user.setUsername(adminName);
-            user.setPassword(passwordEncoder.encode(adminPassword));
-            user.setRole(adminRole);
-            user.setStatus("Active");
-            userRepository.save(user);
-        }
+        createUserIfNotExists(adminName,adminLastname, adminEmail, adminPassword, adminRole);
+        createUserIfNotExists(userName, userLastname, userEmail, userPassword, userRole);
+
     }
 }
