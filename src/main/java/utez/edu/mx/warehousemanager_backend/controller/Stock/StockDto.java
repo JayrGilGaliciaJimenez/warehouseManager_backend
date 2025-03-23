4e22555e37fbd2b1741b5ff3ebcc3920ba2beba1
JayrGilGaliciaJimenez <20223tn052@utez.edu.mx>
2025-03-22 18:59:35 -0600

    feat(stock):
    findAll(), and findByUuid() endpoints added.
    
    Author: JayrGil

---
+++ src/main/java/utez/edu/mx/warehousemanager_backend/controller/Stock/StockDto.java
@@ -0,0 +1,19 @@
+package utez.edu.mx.warehousemanager_backend.controller.Stock;
+
+import lombok.Value;
+
+import java.io.Serializable;
+
+/**
+ * DTO for {@link utez.edu.mx.warehousemanager_backend.model.Stock}
+ */
+@Value
+public class StockDto implements Serializable {
+    String productName;
+    String measurementUnit;
+    int quantity;
+    double unitPrice;
+    double totalAmount;
+    Integer suplierId;
+}
+
