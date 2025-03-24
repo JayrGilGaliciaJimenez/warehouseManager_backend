f6d92f492ee56c96c803669bdc1e1db7532375c5
JayrGilGaliciaJimenez <20223tn052@utez.edu.mx>
2025-03-23 20:12:32 -0600

    feat(ProductOut): implement ProductOut entity, controller, service, and DTO for product management Author: JayrGil

---
+++ src/main/java/utez/edu/mx/warehousemanager_backend/controller/ProductOut/ProductOutController.java
@@ -0,0 +1,33 @@
+package utez.edu.mx.warehousemanager_backend.controller.ProductOut;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import utez.edu.mx.warehousemanager_backend.config.ApiResponse;
+import utez.edu.mx.warehousemanager_backend.model.ProductOut;
+import utez.edu.mx.warehousemanager_backend.service.ProductOutService;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/productOut")
+public class ProductOutController {
+    private final ProductOutService productOutService;
+
+    public ProductOutController(ProductOutService productOutService) {
+        this.productOutService = productOutService;
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<ApiResponse<ProductOut>> save(@RequestBody ProductOutDto dto){
+        return productOutService.save(dto);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<ApiResponse<List<ProductOut>>> findAll(){
+        return productOutService.findAll();
+    }
+
+    @GetMapping("/{uuid}")
+    public ResponseEntity<ApiResponse<ProductOut>> findByUuid(@PathVariable String uuid){
+        return productOutService.findByUuid(uuid);
+    }
+}
---
+++ src/main/java/utez/edu/mx/warehousemanager_backend/service/ProductOutService.java
@@ -0,0 +1,67 @@
+package utez.edu.mx.warehousemanager_backend.service;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import utez.edu.mx.warehousemanager_backend.config.ApiResponse;
+import utez.edu.mx.warehousemanager_backend.controller.ProductOut.ProductOutDto;
+import utez.edu.mx.warehousemanager_backend.model.ProductOut;
+import utez.edu.mx.warehousemanager_backend.repository.ProductOutRepository;
+
+import java.util.List;
+
+@Service
+public class ProductOutService {
+    private final ProductOutRepository productOutRepository;
+
+    public ProductOutService(ProductOutRepository productOutRepository) {
+        this.productOutRepository = productOutRepository;
+    }
+
+    public ResponseEntity<ApiResponse<ProductOut>> save (ProductOutDto dto) {
+        ProductOut savedProductOut = ProductOut.builder()
+                .productName(dto.getProductName())
+                .unitPrice(dto.getUnitPrice())
+                .quantity(dto.getQuantity())
+                .totalAmount(dto.getTotalAmount())
+                .measurementUnit(dto.getMeasurementUnit())
+                .reciverName(dto.getReciverName())
+                .build();
+
+        ApiResponse<ProductOut> response = new ApiResponse<>(
+                productOutRepository.save(savedProductOut),
+                "New product out created",
+                HttpStatus.OK
+        );
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    public ResponseEntity<ApiResponse<ProductOut>> findByUuid(String uuid){
+        if(productOutRepository.findByUuid(uuid) != null){
+            ApiResponse<ProductOut> response = new ApiResponse<>(
+                    productOutRepository.findByUuid(uuid),
+                    "Product out found",
+                    HttpStatus.OK
+            );
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }else{
+            ApiResponse<ProductOut> response = new ApiResponse<>(
+                    "Product out not found",
+                    HttpStatus.NOT_FOUND
+            );
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+    public ResponseEntity<ApiResponse<List<ProductOut>>> findAll() {
+        ApiResponse<List<ProductOut>> response = new ApiResponse<>(
+                productOutRepository.findAll(),
+                "All product outs list",
+                HttpStatus.OK
+        );
+        return new ResponseEntity<>(response, HttpStatus.OK);
+
+    }
+
+}
