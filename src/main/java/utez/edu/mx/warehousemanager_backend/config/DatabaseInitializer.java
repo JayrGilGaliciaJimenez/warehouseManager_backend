5acff1db14ab370ebfb63c6fec0af2546014cd83
JayrGilGaliciaJimenez <20223tn052@utez.edu.mx>
2025-04-01 15:38:23 -0600

    feat(DatabaseInitializer): enhance stock triggers with error handling for insufficient quantity and non-existent products

--- src/main/java/utez/edu/mx/warehousemanager_backend/config/DatabaseInitializer.java
+++ src/main/java/utez/edu/mx/warehousemanager_backend/config/DatabaseInitializer.java
@@ -5,21 +5,23 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+import utez.edu.mx.warehousemanager_backend.service.ProductOutService;
 
 @Component
 public class DatabaseInitializer implements CommandLineRunner {
     private static final Logger logger = LoggerFactory.getLogger(DatabaseInitializer.class);
     private final JdbcTemplate jdbcTemplate;
+    private final ProductOutService productOutService;
 
-    public DatabaseInitializer(JdbcTemplate jdbcTemplate) {
+    public DatabaseInitializer(JdbcTemplate jdbcTemplate, ProductOutService productOutService) {
         this.jdbcTemplate = jdbcTemplate;
+        this.productOutService = productOutService;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
         String createProductEntriesToStockTrigger = """
-                
                  CREATE TRIGGER IF NOT EXISTS product_entries_to_stock
                     AFTER INSERT
                     ON product_entries
@@ -51,41 +53,44 @@ public class DatabaseInitializer implements CommandLineRunner {
                 """;
 
         String createProductOutsToStockTrigger = """
-                
                 CREATE TRIGGER IF NOT EXISTS product_outs_to_stock
-                        AFTER INSERT
-                        ON product_outs
-                        FOR EACH ROW
-                    BEGIN
-                        DECLARE existing_quantity INT;
+                    AFTER INSERT
+                    ON product_outs
+                    FOR EACH ROW
+                BEGIN
+                    DECLARE existing_quantity INT;
                 
-                        -- Check if the product already exists in the stock table
-                        SELECT quantity
-                        INTO existing_quantity
-                        FROM stock
-                        WHERE productName = NEW.productName
-                          AND measurementUnit = NEW.measurementUnit;
+                    -- Check if the product already exists in the stock table
+                    SELECT quantity
+                    INTO existing_quantity
+                    FROM stock
+                    WHERE productName = NEW.productName
+                      AND measurementUnit = NEW.measurementUnit;
                 
-                        IF existing_quantity IS NOT NULL THEN
-                            -- If the product exists, update the quantity and totalAmount
+                    IF existing_quantity IS NULL THEN
+                        -- If the product does not exist, raise an error
+                        SIGNAL SQLSTATE '45000'
+                        SET MESSAGE_TEXT = 'Product does not exist in stock';
+                    ELSEIF existing_quantity < NEW.quantity THEN
+                        -- If the product exists but the quantity is insufficient, raise an error
+                        SIGNAL SQLSTATE '45000'
+                        SET MESSAGE_TEXT = 'Insufficient quantity in stock';
+                    ELSE
+                        -- If the product exists and the quantity is sufficient, update the quantity and totalAmount
                         UPDATE stock
                         SET quantity     = quantity - NEW.quantity,
                             totalAmount = (quantity) * NEW.unitPrice
                         WHERE productName = NEW.productName
                           AND measurementUnit = NEW.measurementUnit;
-                        ELSE
-                            -- If the product does not exist, insert a new record with totalAmount
-                            INSERT INTO stock (id, productName, measurementUnit, quantity, unitPrice, totalAmount)
-                            VALUES (NEW.id, NEW.productName, NEW.measurementUnit, NEW.quantity, NEW.unitPrice,
-                                    NEW.quantity * NEW.unitPrice);
                     END IF;
-                    END;
+                END;
                 """;
 
         try {
             jdbcTemplate.execute(createProductEntriesToStockTrigger);
             logger.info("Trigger 'product_entries_to_stock' created successfully.");
         } catch (Exception e) {
+            productOutService.sqlException(e.getMessage());
             logger.error("Error creating trigger 'product_entries_to_stock': ", e);
         }
 
@@ -93,6 +98,7 @@ public class DatabaseInitializer implements CommandLineRunner {
             jdbcTemplate.execute(createProductOutsToStockTrigger);
             logger.info("Trigger 'product_outs_to_stock' created successfully.");
         } catch (Exception e) {
+            productOutService.sqlException(e.getMessage());
             logger.error("Error creating trigger 'product_outs_to_stock': ", e);
         }
     }
