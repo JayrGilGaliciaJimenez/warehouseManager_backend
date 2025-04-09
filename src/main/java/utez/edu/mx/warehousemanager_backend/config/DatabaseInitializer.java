3e8740c60f2b8b8bfe1bd26f3a0134242c032f1b
JayrGilGaliciaJimenez <20223tn052@utez.edu.mx>
2025-04-08 19:51:32 -0600

    fix(DatabaseInitializer): remove operationId from stock insert statement

--- src/main/java/utez/edu/mx/warehousemanager_backend/config/DatabaseInitializer.java
+++ src/main/java/utez/edu/mx/warehousemanager_backend/config/DatabaseInitializer.java
@@ -45,9 +45,9 @@ public class DatabaseInitializer implements CommandLineRunner {
                       AND measurementUnit = NEW.measurementUnit;
                     ELSE
                         -- If the product does not exist, insert a new record with totalAmount
-                        INSERT INTO stock (id, productName, measurementUnit, quantity, unitPrice, totalAmount, supplierId, operationId)
+                        INSERT INTO stock (id, productName, measurementUnit, quantity, unitPrice, totalAmount, supplierId)
                         VALUES (NEW.id, NEW.productName, NEW.measurementUnit, NEW.quantity, NEW.unitPrice,
-                                NEW.quantity * NEW.unitPrice, NEW.supplierId, NEW.operationId);
+                                NEW.quantity * NEW.unitPrice, NEW.supplierId);
                 END IF;
                 END;
                 """;
