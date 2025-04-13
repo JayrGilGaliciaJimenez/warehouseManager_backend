f19d37cbfc09d46589fc8b9efd7efb114c0c8b41
Leo-Noriega <leonoriega100@gmail.com>
2025-04-13 14:30:57 -0600

    fix(category): remove cascade for entries
    Remove the cascade all for product entries so category can't be deleted if it's used in a product entry

--- src/main/java/utez/edu/mx/warehousemanager_backend/model/Category.java
+++ src/main/java/utez/edu/mx/warehousemanager_backend/model/Category.java
@@ -24,7 +24,7 @@ public class Category {
     private LocalDateTime creationDate = LocalDateTime.now();
     private Integer relatedUserId;
 
-    @OneToMany(mappedBy = "category", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "category", fetch = FetchType.LAZY)
     @JsonIgnore
     private Set<ProductEntry> productEntries;
 
