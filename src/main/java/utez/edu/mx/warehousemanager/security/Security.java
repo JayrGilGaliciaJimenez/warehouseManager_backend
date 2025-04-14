ffbef1cd116b33ac891ee17e433623f9a6709746
Leo-Noriega <leonoriega100@gmail.com>
2025-04-14 11:29:18 -0600

    fix(cors): change cors to stric origin
    changed cors logic to only allowed 1 origin from de application.properties

--- src/main/java/utez/edu/mx/warehousemanager/security/Security.java
+++ src/main/java/utez/edu/mx/warehousemanager/security/Security.java
@@ -1,5 +1,7 @@
-package utez.edu.mx.warehousemanager_backend.security;
+package utez.edu.mx.warehousemanager.security;
 
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,9 +17,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import jakarta.servlet.http.HttpServletResponse;
-import utez.edu.mx.warehousemanager_backend.jwt.JwtTokenFilter;
+import utez.edu.mx.warehousemanager.jwt.JwtTokenFilter;
 
 import java.util.List;
 
@@ -44,8 +44,8 @@ public class Security {
     }
 
     private static final String USER = "USER";
-    private static final  String ADMIN = "ADMIN";
-    
+    private static final String ADMIN = "ADMIN";
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
@@ -55,26 +55,28 @@ public class Security {
                                 (request, response, ex) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED)));
         http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
         http.authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/api/auth/login").permitAll()
-                .requestMatchers("/api/auth/reset-email").permitAll()
-                .requestMatchers("/api/auth/reset-password/**").permitAll()
-                .requestMatchers("/api/auth/activate-account/**").permitAll()
-                .requestMatchers("/api/user").hasRole(ADMIN)
-                .requestMatchers("/api/category").hasRole(USER)
-                .requestMatchers("/api/supplier").hasAnyRole(USER, ADMIN)
-                .requestMatchers("/api/productEntry").hasAnyRole(USER, ADMIN)
-                .requestMatchers("/api/stock").hasRole(USER)
-                .requestMatchers("/api/productOut").hasAnyRole(USER, ADMIN)
-
-                .anyRequest().authenticated());
-        http.cors().configurationSource(corsConfigurationSource());
+                        .requestMatchers("/api/auth/login").permitAll()
+                        .requestMatchers("/api/auth/reset-email").permitAll()
+                        .requestMatchers("/api/auth/reset-password/**").permitAll()
+                        .requestMatchers("/api/auth/activate-account/**").permitAll()
+                        .requestMatchers("/api/user").hasRole(ADMIN)
+                        .requestMatchers("/api/category").hasRole(USER)
+                        .requestMatchers("/api/supplier").hasAnyRole(USER, ADMIN)
+                        .requestMatchers("/api/productEntry").hasAnyRole(USER, ADMIN)
+                        .requestMatchers("/api/stock").hasRole(USER)
+                        .requestMatchers("/api/productOut").hasAnyRole(USER, ADMIN)
+                        .anyRequest().authenticated())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()));
         return http.build();
     }
+
+    @Value("${cors.allowed.origin}")
+    private String allowedOrigin;
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of(allowedOrigin));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
